bfd7a04576fe3d89b35eb596b62b37ef1e533972
geniy9 <geniy9@gmail.com>
2026-05-18 23:03:10 +0500

    fb-pixel

--- app/app.vue
+++ app/app.vue
@@ -1,28 +1,39 @@
 <script setup>
+const route = useRoute()
 useHead({
   titleTemplate: (title) => {
     return title ? `${title} | LIGHT FREIGHT` : "LIGHT FREIGHT"
   },
   script: [
     {
-      children: `!function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '1982855955827405');
-      fbq('track', 'PageView');`,
-      type: 'text/javascript'
-    }
+      id: 'fb-pixel',
+      innerHTML: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1982855955827405');
+        fbq('track', 'PageView');
+      `,
+      type: 'text/javascript',
+    },
   ],
   noscript: [
     {
-      children: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1982855955827405&ev=PageView&noscript=1" />`
+      children: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1982855955827405&ev=PageView&noscript=1" />`,
+      body: true
     }
   ]
+})
+
+watch(() => route.fullPath, () => {
+  if (typeof window !== 'undefined' && window.fbq) {
+    window.fbq('track', 'PageView')
+  }
 })
 </script>
 <template>
