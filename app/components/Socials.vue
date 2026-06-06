35b19e5e93621f0fa144f2898cfdda16b478e480
geniy9 <geniy9@gmail.com>
2026-06-06 18:47:02 +0500

    Socials repeat upd

--- app/components/Socials.vue
+++ app/components/Socials.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <a href="https://wa.me/19169687082" target="_blank" class="link_social">
-      <Icon name="hugeicons:whatsapp" />
+      <UIcon name="hugeicons:whatsapp" />
     </a>
     <a href="https://www.facebook.com/LightFreights" target="_blank" class="link_social">
-      <Icon name="hugeicons:facebook-01" />
+      <UIcon name="hugeicons:facebook-01" />
     </a>
     <a href="https://www.instagram.com/light_freight_dispatch" target="_blank" class="link_social">
-      <Icon name="hugeicons:instagram" />
+      <UIcon name="hugeicons:instagram" />
     </a>
   </div>
 </template>
